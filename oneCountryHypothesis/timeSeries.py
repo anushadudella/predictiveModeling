c434edfe75c97640ea464639a75f499ef4136924
Anusha Dudella <anusharao4262@gmail.com>
2023-07-23 11:49:41 -0500

    one country search term analysis

---
+++ oneCountryHypothesis/timeSeries.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+from pytrends.request import TrendReq
+from pathlib import Path
+import pandas as pd
+
+
+def getGoogleArray(keyword1, date1, date2, GEOPARAM):
+    pytrend = TrendReq()
+    pytrend.build_payload(kw_list=[keyword1], timeframe=[date1 + ' ' + date2], geo=GEOPARAM)
+    df2 = pytrend.interest_over_time()
+    filepath = Path(DATAPATH + keyword1 + '_' + date1 + '_' + date2 + '_' + 'Data.csv')
+    df2.to_csv(filepath)
+    return df2
+
+
+
+def getDataFrame(keyword1, date1, date2, GEOPARAM):
+    df3 = getGoogleArray(keyword1, date1, date2, GEOPARAM)
+    dftimes1 = df3.index.tolist()
+    dfkeyword1 = df3[keyword1].tolist()
+    time_keyword1 = pd.DataFrame(
+    {'Time': dftimes1,
+     'Depression': dfkeyword1,
+    })
+    return time_keyword1
+
+
+    # print(dfkeyword)
+    return dfkeyword
+
+
+keyword2 = 'Depression'
+geoparam = 'US'
+date1 = '2020-04-04'
+date2 = '2021-04-06'
+date3 = '2018-04-04'
+date4 = '2019-04-06'
+DATAPATH = '/home/adudella/PycharmProjects/predictiveModeling/oneCountryHypothesis/' + keyword2 + '/'
+
+try:
+    time_keyword1 = getDataFrame(keyword2, date1, date2, geoparam)
+    # print(time_keyword1)
+    time_keyword2 = getDataFrame(keyword2, date3, date4, geoparam)
+    print(time_keyword2)
+
+    ax = time_keyword1.plot(x='Time', y='Depression')
+    time_keyword2.plot(ax=ax, x='Time', y='Depression')
+    plt.show()
+except Exception as e:
+    print(e.args)
+
+# time_keyword1 = getDataFrame('Depression','2020-04-04', '2021-04-06')
+# time_keyword2 = getDataFrame('Depression','2018-04-04', '2019-04-06')
+# ax = time_keyword1.plot(x='Time', y='Depression')
+# time_keyword2.plot(ax=ax, x='Time', y='Depression')
+#
+
