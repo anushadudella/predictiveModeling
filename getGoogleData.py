3e3879fe463d5c67573c48765a2cb9fbc7ce0862
Anusha Dudella <anusharao4262@gmail.com>
2023-07-23 09:25:10 -0500

    added geolocation

--- getGoogleData.py
+++ getGoogleData.py
@@ -1,49 +1,56 @@
-import pandas as pd
-from pytrends.request import TrendReq
-from numpy import mean
-from sklearn.linear_model import LinearRegression
-from numpy import std
-from pathlib import Path
+import time
 
-def getGoogleArray(keyword1, keyword2, date1, date2):
+import matplotlib.pyplot as plt
+import numpy
+from pytrends.request import TrendReq
+from pathlib import Path
+import pandas as pd
+
+
+def getGoogleArray(keyword1, date1, date2, GEOPARAM):
+    #GEOPARAM = 'US-NY'
     pytrend = TrendReq()
-    pytrend.build_payload(kw_list=[keyword1], timeframe=[date1 + ' ' + date2])
+    pytrend.build_payload(kw_list=[keyword1], timeframe=[date1 + ' ' + date2], geo=GEOPARAM)
     df2 = pytrend.interest_over_time()
-    print(df2)
-    filepath = Path('/home/adudella/PycharmProjects/googleTrends/BulimiaData.csv')
+    filepath = Path('./' + keyword1 + 'Data.csv')
     df2.to_csv(filepath)
 
-    pytrend = TrendReq()
-    pytrend.build_payload(kw_list=[keyword2], timeframe=[date1 + ' ' + date2])
-    df3 = pytrend.interest_over_time()
-    filepath = Path('/home/adudella/PycharmProjects/googleTrends/COVIDData.csv')
-    df3.to_csv(filepath)
-
-    return df2, df3
-
-def getRSquared(df, keyword1, keyword2):
-    corr = df[keyword1].corr(df[keyword2])
-    return corr
-
-def getGoogleSpecificDataFrame1(keyword1, date1, date2):
-    pytrend = TrendReq()
-    pytrend.build_payload(kw_list=[keyword1], timeframe=[date1 + ' ' + date2])
-    df2 = pytrend.interest_over_time()
-    # filepath = Path('/home/adudella/PycharmProjects/googleTrends/BulimiaData.csv')
-    # df2.to_csv(filepath)
-
-    x = df2.to_numpy()
     return df2
-def getGoogleSpecificDataFrame2(keyword2, date1, date2):
-    pytrend = TrendReq()
-    pytrend.build_payload(kw_list=[keyword2], timeframe=[date1 + ' ' + date2])
-    df3 = pytrend.interest_over_time()
-    # filepath = Path('/home/adudella/PycharmProjects/googleTrends/COVIDData.csv')
-    # df3.to_csv(filepath)
 
-    y = df3.to_numpy()
-    return df3
+def getDataFrame(keyword1, date1, date2, GEOPARAM):
+    df3 = getGoogleArray(keyword1, date1, date2, GEOPARAM)
+    #print(df3)
+    dfkeyword = df3[keyword1].to_frame()
 
-z = getGoogleSpecificDataFrame1('bulimia', '2020-04-04', '2021-04-06')
-print(z)
+    return dfkeyword
+
+#Corona Depression 0.64
+#Corona Anxiety 0.4
+#Covid 19 depression 0.53
+#Covid 19 Anxiety 0.39
+#Covid 19 Death 0.83
+#Covid 19 Suicide -0.083
+#Covid 19 Cough 0.411
+#Covid 19 Blues -0.26
+#Covid 19 Bipolar 0.26
+#Covid 19 Anorexia 0.35
+#Covid 19 Bulimia 0.058
+#Covid 19 adhd -0.71
+
+
+keyword1 = 'Covid 19'
+keyword2_list = ['Autism', 'Death','Depression', 'Anxiety']
+geoparam = ['US-FL','US-NY','US-NJ']
+
+try:
+    for geocode in geoparam:
+        for keyword2 in keyword2_list:
+            time_keyword1 = getDataFrame(keyword1,'2020-04-04', '2021-04-06', geocode)
+            time_keyword2 = getDataFrame(keyword2,'2020-04-04', '2021-04-06', geocode)
+            list1 = time_keyword1[keyword1].to_list()
+            list2 = time_keyword2[keyword2].to_list()
+            print(keyword1 + ',' + keyword2 + ',' + geocode + ',' + str(numpy.corrcoef(list1,list2).flat[1]))
+            time.sleep(20)
+except Exception as e:
+    print(e.args)
 
