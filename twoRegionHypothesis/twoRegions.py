ef95ce662352f86ba40bbfe0dcb968be8189e242
Anusha Dudella <anusharao4262@gmail.com>
2023-07-26 15:48:24 -0500

    cleaned up data, files, & folders

--- twoRegionHypothesis/twoRegions.py
+++ twoRegionHypothesis/twoRegions.py
@@ -6,7 +6,7 @@ def getGoogleArray(keyword1, date1, date2, GEOPARAM):
     pytrend = TrendReq()
     pytrend.build_payload(kw_list=[keyword1], timeframe=[date1 + ' ' + date2], geo=GEOPARAM)
     df2 = pytrend.interest_over_time()
-    # filepath = Path('/Users/kkaa_austin/PycharmProjects/predictiveModeling/COVIDCasesAndKeywords' + keyword1 + 'Data.csv')
+    # filepath = Path('/Users/kkaa_austin/PycharmProjects/predictiveModeling/covidCasesKeywords' + keyword1 + 'Data.csv')
     # df2.to_csv(filepath)
     return df2
 
