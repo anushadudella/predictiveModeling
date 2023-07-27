6b3b34e73dd2cb7f3a525f8e1c2fe818ec616081
Anusha Dudella <anusharao4262@gmail.com>
2023-07-27 09:38:38 -0500

    worked on euclidian distance

--- oneCountryHypothesis/oneRegion_with_euclidean.py
+++ oneCountryHypothesis/oneRegion_with_euclidean.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from sklearn.metrics.pairwise import euclidean_distances
 # import seaborn as sns
 # import pandas as pd
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 import pandas as pd
 
 
@@ -15,9 +15,9 @@ def getGoogleArray(keyword1, date1, date2):
     pytrend = TrendReq()
     pytrend.build_payload(kw_list=[keyword1], timeframe=[date1 + ' ' + date2])
     df2 = pytrend.interest_over_time()
-    filepath = Path('/home/adudella/PycharmProjects/predictiveModeling/oneCountryHypothesis/' + keyword1
-                    + '_' + date1 + '_' + date2 + '_' + 'Data.csv')
-    df2.to_csv(filepath)
+    # filepath = Path('/Users/kkaa_austin/PycharmProjects/PredictCovid/' + keyword1
+    #                 + '_' + date1 + '_' + date2 + '_' + 'Data.csv')
+    # df2.to_csv(filepath)
     return df2
 
 
@@ -32,11 +32,14 @@ def getDataFrame(keyword1, date1, date2):
     return time_keyword1
 
 
-time_keyword1 = getDataFrame('depression', '2018-04-04', '2019-04-06')
-time_keyword2 = getDataFrame('depression', '2020-04-05', '2021-04-06')
+time_keyword1 = getDataFrame('depression', '2018-04-08', '2019-04-06')
+time_keyword2 = getDataFrame('depression', '2020-04-05', '2021-04-03')
 
 time1 = time_keyword1.index.tolist()
+time1_series = time_keyword1.index
 keyword1 = time_keyword1['Keyword'].tolist()
+keyword1_series = time_keyword1['Keyword']
+
 time1_keyword1_2018_19 = pd.DataFrame(
     {'Time': time1,
      'Depression 2018-2019': keyword1,
@@ -44,6 +47,8 @@ time1_keyword1_2018_19 = pd.DataFrame(
 
 time2 = time_keyword2.index.tolist()
 keyword2 = time_keyword2['Keyword'].tolist()
+keyword2_series = time_keyword2['Keyword']
+
 time2_keyword2_2020_21 = pd.DataFrame(
     {'Time': time2,
      'Depression 2020-2021': keyword2,
@@ -54,13 +59,18 @@ time2_keyword2_2020_21.plot(ax=ax, x='Time', y='Depression 2020-2021')
 
 plt.xlabel('Time in Weeks')
 plt.ylabel('Freq. of Keywords')
-plt.show()
+#plt.show()
 
-# find the euclidean distances
 
 # first create a dataframe with 2 columns of data from the
 # 2 timeframes we have above time1_keyword1_2020_21 and time1_keyword1_2018_19
 # Now the data frame will look like 2018_19_Depression and 2020_21_Depression by week
 
-euc_dist = euclidean_distances(smoothed.T)
-pd.DataFrame(euc_dist, index=selected, columns=selected)
+my_df_dict2 = {'2018_19' : keyword1_series , '2020_21' : keyword2_series}
+
+my_df = pd.DataFrame(my_df_dict2)
+# print(my_df)
+
+euc_dist = euclidean_distances(my_df.T)
+print(euc_dist)
+#pd.DataFrame(euc_dist, index=selected, columns=selected)
